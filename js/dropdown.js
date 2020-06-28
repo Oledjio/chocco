a67f4935cadc324223e32fe8390df85dcfd3fdfa
Oleg Seliverstov <oleg.seliverstov94@gmail.com>
2020-06-28 16:50:21 +0200

    реализован функционал выпадающего меню

---
+++ js/dropdown.js
@@ -0,0 +1,12 @@
+let gamburgerMenu = document.querySelector('.menu-icon');
+let closeMenu = document.querySelector('.dropdown__close');
+let dropdownMenu = document.querySelector('.dropdown');
+
+
+gamburgerMenu.addEventListener('click', function(e) {
+    dropdownMenu.classList.add('dropdown__active')
+});
+closeMenu.addEventListener('click', function(evt){
+    dropdownMenu.classList.remove('dropdown__active')
+});
+
