b7697fc534183b07b617c7d8b909133221800f88
Oleg Seliverstov <oleg.seliverstov94@gmail.com>
2020-07-03 18:07:06 +0200

    slick slider

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,18 @@
 
-const slider = $('.slider').bxSlider({
-    pager: false,
-    controls: false
+// const slider = $('.slider').bxSlider({
+//     pager: false,
+//     controls: false
 
+// });
+
+
+const slider = $('.slider').slick({
+    prevArrow: $('.arrow__link_prev'),
+    nextArrow: $('.arrow__link_next')
 });
-
 $('.arrow__link_prev').click(e =>{
     e.preventDefault();
-    slider.goToPrevSlide();
 })
 $('.arrow__link_next').click(e =>{
     e.preventDefault();
-    slider.goToNextSlide();
 });
