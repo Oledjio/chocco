3ae2aac5553d7da1bec4d518f97b02bbaa763c01
Oleg Seliverstov <oleg.seliverstov94@gmail.com>
2020-07-08 19:54:21 +0200

    onepagescroll

--- js/onepagescroll.js
+++ js/onepagescroll.js
@@ -20,7 +20,7 @@ scroll.addEventListener('wheel', function(e) {
     }
     
     scrollToSection(currentSection);
-    activeClass(currentSection)
+    activeClass(currentSection);
 });
 
 function scrollToSection(i) {
@@ -35,4 +35,31 @@ function activeClass(index){
         }
         pager[index].classList.add('pager__item_active');
     }
+}
+
+//пролистывание по клику
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    
+    anchor.addEventListener('click', function (e) {
+    console.log(anchors);
+    e.preventDefault();
+    let target = e.target;
+    const item = target.closest('.pager__item')
+    
+    if(!item.classList.contains('pager__item_active')){
+        for (let i = 0; i<pager.length; i++){
+            pager[i].classList.remove('pager__item_active');
+        }
+        item.classList.add('pager__item_active');
+    }   
+    const blockID = anchor.getAttribute('href').substr(1);
+    console.log(blockID);
+    document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        });
+    });
+    
 }
