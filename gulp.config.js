46a4504f6796c2879ed4931adeda9e99aaa0e76a
Oleg Seliverstov <oleg.seliverstov94@gmail.com>
2020-07-15 14:02:02 +0200

    исправленный touchswipe

--- gulp.config.js
+++ gulp.config.js
@@ -3,13 +3,13 @@ module.exports = {
     DIST_PATH : 'dist',
     STYLES_LIBS : [
         "node_modules/normalize.css/normalize.css",
-        "src/libs/slick/slick.css",
+        "src/libs/slick/slick.css"
     ],
 
     JS_LIBS : [
         "node_modules/jquery/dist/jquery.js",
         "src/libs/slick/slick.min.js",
         "node_modules/mobile-detect/mobile-detect.js",
-        "node_modules/jquery-touchswipe/jquery.touchSwipe.js",
+        "node_modules/jquery-touchswipe/jquery.touchSwipe.js"
     ]
 }
